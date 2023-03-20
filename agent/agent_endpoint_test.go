80dede8717f1467f759fbe8d2ea166624e2a9886
Chris S. Kim <ckim@hashicorp.com>
2023-03-20 11:25:29 -0400

    Support subsystem levels for monitor command

--- agent/agent_endpoint_test.go
+++ agent/agent_endpoint_test.go
@@ -18,7 +18,6 @@ import (
 	"time"
 
 	"github.com/armon/go-metrics"
-	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/serf/serf"
@@ -37,7 +36,7 @@ import (
 	"github.com/hashicorp/consul/agent/local"
 	"github.com/hashicorp/consul/agent/structs"
 	"github.com/hashicorp/consul/agent/token"
-	tokenStore "github.com/hashicorp/consul/agent/token"
+	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/envoyextensions/xdscommon"
 	"github.com/hashicorp/consul/lib"
 	"github.com/hashicorp/consul/sdk/testutil"
@@ -1598,7 +1597,7 @@ func TestAgent_Metrics_ACLDeny(t *testing.T) {
 
 func TestHTTPHandlers_AgentMetricsStream_ACLDeny(t *testing.T) {
 	bd := BaseDeps{}
-	bd.Tokens = new(tokenStore.Store)
+	bd.Tokens = new(token.Store)
 	sink := metrics.NewInmemSink(30*time.Millisecond, time.Second)
 	bd.MetricsConfig = &lib.MetricsConfig{
 		Handler: sink,
@@ -1627,7 +1626,7 @@ func TestHTTPHandlers_AgentMetricsStream_ACLDeny(t *testing.T) {
 
 func TestHTTPHandlers_AgentMetricsStream(t *testing.T) {
 	bd := BaseDeps{}
-	bd.Tokens = new(tokenStore.Store)
+	bd.Tokens = new(token.Store)
 	sink := metrics.NewInmemSink(20*time.Millisecond, time.Second)
 	bd.MetricsConfig = &lib.MetricsConfig{
 		Handler: sink,
@@ -6112,6 +6111,50 @@ func TestAgent_Monitor(t *testing.T) {
 			t.Fatalf("got %q and did not find %q", got, want)
 		}
 	})
+
+	t.Run("logsublevel invalid format", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/v1/agent/monitor?loglevel=warn&logsublevel=debug", nil)
+
+		resp := httptest.NewRecorder()
+		a.srv.h.ServeHTTP(resp, req)
+		require.Equal(t, http.StatusBadRequest, resp.Code)
+
+		expect := "Expected sublevel to be `<subsystem>:<log-level>` format"
+		require.Contains(t, resp.Body.String(), expect)
+	})
+
+	t.Run("logsublevel valid format", func(t *testing.T) {
+		cancelCtx, cancelFunc := context.WithCancel(context.Background())
+		req, _ := http.NewRequestWithContext(cancelCtx, "GET", "/v1/agent/monitor?loglevel=warn&logsublevel=TestAgent.http:debug", nil)
+
+		resp := httptest.NewRecorder()
+		go a.srv.h.ServeHTTP(resp, req)
+
+		args := &structs.ServiceDefinition{
+			Name: "monitor",
+			Port: 8000,
+			Check: structs.CheckType{
+				TTL: 15 * time.Second,
+			},
+		}
+
+		registerReq, _ := http.NewRequest("PUT", "/v1/agent/service/register", jsonReader(args))
+		res := httptest.NewRecorder()
+		a.srv.h.ServeHTTP(res, registerReq)
+		if http.StatusOK != res.Code {
+			t.Fatalf("expected 200 but got %v", res.Code)
+		}
+
+		require.Eventually(t, func() bool {
+			return len(resp.Body.Bytes()) > 0
+		}, 3*time.Second, 100*time.Millisecond)
+		expect := "TestAgent.http: Request finished: method=PUT url=/v1/agent/service/register"
+		require.Contains(t, resp.Body.String(), expect, "expected debug level message but got none")
+		notExpect := "Synced service: service=monitor"
+		require.NotContains(t, resp.Body.String(), notExpect, "got info level message but expected none")
+
+		cancelFunc()
+	})
 }
 
 func TestAgent_Monitor_ACLDeny(t *testing.T) {
@@ -6291,15 +6334,15 @@ func TestAgent_Token(t *testing.T) {
 
 	type tokens struct {
 		user                string
-		userSource          tokenStore.TokenSource
+		userSource          token.TokenSource
 		agent               string
-		agentSource         tokenStore.TokenSource
+		agentSource         token.TokenSource
 		agentRecovery       string
-		agentRecoverySource tokenStore.TokenSource
+		agentRecoverySource token.TokenSource
 		repl                string
-		replSource          tokenStore.TokenSource
+		replSource          token.TokenSource
 		registration        string
-		registrationSource  tokenStore.TokenSource
+		registrationSource  token.TokenSource
 	}
 
 	resetTokens := func(init tokens) {
@@ -6350,7 +6393,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "acl_token",
 			body:      body("U"),
 			code:      http.StatusOK,
-			raw:       tokens{user: "U", userSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{user: "U", userSource: token.TokenSourceAPI},
 			effective: tokens{user: "U", agent: "U"},
 		},
 		{
@@ -6359,7 +6402,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "default",
 			body:      body("U"),
 			code:      http.StatusOK,
-			raw:       tokens{user: "U", userSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{user: "U", userSource: token.TokenSourceAPI},
 			effective: tokens{user: "U", agent: "U"},
 		},
 		{
@@ -6369,7 +6412,7 @@ func TestAgent_Token(t *testing.T) {
 			body:      body("A"),
 			code:      http.StatusOK,
 			init:      tokens{user: "U", agent: "U"},
-			raw:       tokens{user: "U", agent: "A", agentSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{user: "U", agent: "A", agentSource: token.TokenSourceAPI},
 			effective: tokens{user: "U", agent: "A"},
 		},
 		{
@@ -6379,7 +6422,7 @@ func TestAgent_Token(t *testing.T) {
 			body:      body("A"),
 			code:      http.StatusOK,
 			init:      tokens{user: "U", agent: "U"},
-			raw:       tokens{user: "U", agent: "A", agentSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{user: "U", agent: "A", agentSource: token.TokenSourceAPI},
 			effective: tokens{user: "U", agent: "A"},
 		},
 		{
@@ -6388,7 +6431,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "acl_agent_master_token",
 			body:      body("M"),
 			code:      http.StatusOK,
-			raw:       tokens{agentRecovery: "M", agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:       tokens{agentRecovery: "M", agentRecoverySource: token.TokenSourceAPI},
 			effective: tokens{agentRecovery: "M"},
 		},
 		{
@@ -6397,7 +6440,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "agent_master",
 			body:      body("M"),
 			code:      http.StatusOK,
-			raw:       tokens{agentRecovery: "M", agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:       tokens{agentRecovery: "M", agentRecoverySource: token.TokenSourceAPI},
 			effective: tokens{agentRecovery: "M"},
 		},
 		{
@@ -6406,8 +6449,8 @@ func TestAgent_Token(t *testing.T) {
 			url:       "agent_recovery",
 			body:      body("R"),
 			code:      http.StatusOK,
-			raw:       tokens{agentRecovery: "R", agentRecoverySource: tokenStore.TokenSourceAPI},
-			effective: tokens{agentRecovery: "R", agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:       tokens{agentRecovery: "R", agentRecoverySource: token.TokenSourceAPI},
+			effective: tokens{agentRecovery: "R", agentRecoverySource: token.TokenSourceAPI},
 		},
 		{
 			name:      "set repl legacy",
@@ -6415,7 +6458,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "acl_replication_token",
 			body:      body("R"),
 			code:      http.StatusOK,
-			raw:       tokens{repl: "R", replSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{repl: "R", replSource: token.TokenSourceAPI},
 			effective: tokens{repl: "R"},
 		},
 		{
@@ -6424,7 +6467,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "replication",
 			body:      body("R"),
 			code:      http.StatusOK,
-			raw:       tokens{repl: "R", replSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{repl: "R", replSource: token.TokenSourceAPI},
 			effective: tokens{repl: "R"},
 		},
 		{
@@ -6433,7 +6476,7 @@ func TestAgent_Token(t *testing.T) {
 			url:       "config_file_service_registration",
 			body:      body("G"),
 			code:      http.StatusOK,
-			raw:       tokens{registration: "G", registrationSource: tokenStore.TokenSourceAPI},
+			raw:       tokens{registration: "G", registrationSource: token.TokenSourceAPI},
 			effective: tokens{registration: "G"},
 		},
 		{
@@ -6443,7 +6486,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{user: "U"},
-			raw:    tokens{userSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{userSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear default",
@@ -6452,7 +6495,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{user: "U"},
-			raw:    tokens{userSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{userSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear agent legacy",
@@ -6461,7 +6504,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{agent: "A"},
-			raw:    tokens{agentSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{agentSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear agent",
@@ -6470,7 +6513,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{agent: "A"},
-			raw:    tokens{agentSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{agentSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear master legacy",
@@ -6479,7 +6522,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{agentRecovery: "M"},
-			raw:    tokens{agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:    tokens{agentRecoverySource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear master",
@@ -6488,7 +6531,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{agentRecovery: "M"},
-			raw:    tokens{agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:    tokens{agentRecoverySource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear recovery",
@@ -6497,7 +6540,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{agentRecovery: "R"},
-			raw:    tokens{agentRecoverySource: tokenStore.TokenSourceAPI},
+			raw:    tokens{agentRecoverySource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear repl legacy",
@@ -6506,7 +6549,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{repl: "R"},
-			raw:    tokens{replSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{replSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear repl",
@@ -6515,7 +6558,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{repl: "R"},
-			raw:    tokens{replSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{replSource: token.TokenSourceAPI},
 		},
 		{
 			name:   "clear registration",
@@ -6524,7 +6567,7 @@ func TestAgent_Token(t *testing.T) {
 			body:   body(""),
 			code:   http.StatusOK,
 			init:   tokens{registration: "G"},
-			raw:    tokens{registrationSource: tokenStore.TokenSourceAPI},
+			raw:    tokens{registrationSource: token.TokenSourceAPI},
 		},
 	}
 	for _, tt := range tests {
